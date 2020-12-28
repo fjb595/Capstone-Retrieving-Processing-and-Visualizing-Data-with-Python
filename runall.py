7eb0e2573402a60a97702917c8efd5d9654bad3e
fjb595 <33468371+fjb595@users.noreply.github.com>
2020-12-27 18:38:02 -0800

    Add files via upload

--- runall.py
+++ runall.py
@@ -1,6 +1,21 @@
 
 import webbrowser
 import os
+from pathlib import Path
+
+
+#
+# produces a chart comparing US covid-19 fatalities with other top causes of death
+#
+# this script is provided as convenience.  the imported scripts can also be executed
+# manually
+#
+# see the imported scripts for details of their operation
+#
+
+
+# scripts assume current dir is script dir
+os.chdir(Path(__file__).parent)
 
 # perform all processing
 
@@ -8,6 +23,8 @@ import scandl
 import querydb
 import covid
 
-# display the visualization
+
+
+# if the chart isn't displayed, open top10.html in a web browser
 webbrowser.open('file://' +os.path.realpath(os.path.join(".\\","top10.html")).replace("\\","/"))
 
